69048dcbed625d69e96668a7231b6d67f7eac6d6
Heiko Westermann <hwt3@gmx.de>
2017-04-07 00:13:38 +0200

    Feature: Added offset for width difference

--- src/hwt.jquery.resizeobserver.js
+++ src/hwt.jquery.resizeobserver.js
@@ -11,23 +11,29 @@
  * in your document ready function to use the resize observer.
  */
 
-function resizeObserver(resizeCallback, resizeRefreshTime) {
+function resizeObserver(resizeCallback, resizeRefreshTime, resizeOffsetX) {
 	"use strict";
-    
+
     resizeCallback = resizeCallback || function () {};
     resizeRefreshTime = resizeRefreshTime || 200;
-	
+    resizeOffsetX = resizeOffsetX || 5;
+
 	var resizeDelay, resizeHandler;
 	var lastWindowWidth = jQuery(window).width();
-	
+    var newWindowWidth;
+
 	resizeHandler = function () {
-		if (jQuery(window).width() !== lastWindowWidth) {
+        newWindowWidth = jQuery(window).width();
+        console.log(Math.abs(newWindowWidth-lastWindowWidth));
+		if ( (newWindowWidth !== lastWindowWidth) && (Math.abs(newWindowWidth-lastWindowWidth) > resizeOffsetX) )  {
+            console.log('executed');
 			window.clearTimeout(resizeDelay);
 			resizeDelay = window.setTimeout(function () {
-				lastWindowWidth = jQuery(window).width();
+				lastWindowWidth = newWindowWidth;
 				resizeCallback.call(this);
 			}, resizeRefreshTime);
 		}
 	};
+
 	jQuery(window).resize(resizeHandler);
 }
